effae46a057bc4af9da13ed0c0d2b19dfb332302
Kat <nelson.katherine.24@c-dh.org>
2023-09-15 09:10:16 -0500

    005 upload media stats

---
+++ 004_MediaStats/MediaStats.java
@@ -0,0 +1,20 @@
+
+public class MediaStats
+{
+    public static void main(String[] args)
+    {
+        int beforeTheCoffeeGetsColdReleaseYear = 2015;
+        int beforeTheCoffeeGetsColdPages = 272;
+        int beautifulBoyReleaseYear = 2008;
+        int beautifulBoyPages = 352;
+        int faithHopeAndCarnargeReleaseYear = 2022;
+        int faithHopeAndCarnargePages =  304;
+
+        // this is a comment-- command k + command t = change theme
+        // this is for using long comments ----> /*  xx comment here */
+
+        System.out.println("The book Before The Coffee Gets Cold was released in " + beforeTheCoffeeGetsColdReleaseYear + ", and has " + beforeTheCoffeeGetsColdPages + " pages." );
+        System.out.println("The book Beautiful Boy was released in " + beautifulBoyReleaseYear + ", and has " + beautifulBoyPages + " pages." );
+        System.out.println("The book Faith Hope and Carnage was released in " + faithHopeAndCarnargeReleaseYear + ", and has " + faithHopeAndCarnargePages + " pages." );
+    }
+}
