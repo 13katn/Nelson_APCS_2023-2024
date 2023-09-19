1b522cde8cacafa71f5941caa6ae7ca17756561a
Kat <nelson.katherine.24@c-dh.org>
2023-09-19 11:11:41 -0500

    005 table of variables

---
+++ 005_ATableOfVariables/ATableOfVariables.java
@@ -0,0 +1,33 @@
+public class ATableOfVariables 
+{
+    public static void main(String[] args)
+    {
+        // SET A:
+        int rowOneColumnTwo = 1;
+        int rowTwoColumnTwo = 4;
+        int rowThreeColumnTwo = 9;
+        int rowFourColumnTwo = 16;
+        // this was the first highlited column, so every squared result is up there, callign it set A^^^
+
+        // SET B
+        int rowOneColumnThree = 1;
+        int rowTwoColumnThree = 8;
+        int rowThreeColumnThree = 27;
+        int rowFourColumnThree = 64;
+        // ^^^  this is the lsit of all the results after the row numbers were cubed, calling it set B^^
+
+        // SET A AND SET B PRTINED BELOW
+        System.out.println(rowOneColumnTwo);
+        System.out.println(rowTwoColumnTwo);
+        System.out.println(rowThreeColumnTwo);
+        System.out.println(rowFourColumnTwo);
+       // end set a print begin set b print
+        System.out.println(rowOneColumnThree);
+        System.out.println(rowTwoColumnThree);
+        System.out.println(rowThreeColumnThree);
+        System.out.println(rowFourColumnThree);
+ 
+    }
+}
+
+        
