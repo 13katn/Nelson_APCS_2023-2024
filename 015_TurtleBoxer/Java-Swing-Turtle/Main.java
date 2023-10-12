655e39072de76c7dd6833bc454f542e98d25454b
Kat <nelson.katherine.24@c-dh.org>
2023-10-12 09:26:32 -0500

    15 turtle box

--- 015_TurtleBoxer/Java-Swing-Turtle/Main.java
+++ 015_TurtleBoxer/Java-Swing-Turtle/Main.java
@@ -15,11 +15,61 @@ public class Main {
     |   |   |
     |___|___|
     |   |   |
-    |___|___|
+    |___|___| 
 
     */
 
+  
 
+        World size = new World(220,220);
+        Turtle turtleBob = new Turtle(size);
+
+        // first top right square 
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+
+        turtleBob.forward();
+        turtleBob.turnLeft();
+        turtleBob.forward();
+        turtleBob.turnLeft();
+        turtleBob.forward();
+        turtleBob.turnLeft();
+        turtleBob.forward();
+
+        turtleBob.turnRight();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+// top left
+        turtleBob.turnLeft();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+        turtleBob.turnRight();
+        turtleBob.forward();
+
+
+
+
+
+        // second square top left
+
+        // third square bottom right
+
+        //third square bottom left
+
+        size.show(true);
     
     //Finally, set the world to be visible with a boolean. Ex: worldName.setVisible(the proper boolean);
   }
