b5bf115cdea462cb8d57f4cf6e6ffba43e8dfcaf
Kat <nelson.katherine.24@c-dh.org>
2023-11-02 10:42:29 -0500

    assingments 15-22

---
+++ 021_CarBuilderOne/CarBuilderOne.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CarBuilderOne {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        String carMake = "";
+        String carModel = "";
+        int carYear = 0;
+        int carMileage = 0;
+
+
+        System.out.println("What is your ideal make for your car?");
+        carMake = scan.nextLine();
+
+        System.out.println("What is your ideal model for your car?");
+        carModel = scan.nextLine();
+    
+        System.out.println("What is your ideal year for your new car?");
+        scan.nextLine();
+        carYear = scan.nextInt();
+
+        System.out.println("What is your ideal mileage for your new car?");
+        carMileage = scan.nextInt();
+
+        Car foo = new Car(carYear, carMileage, carMake, carModel);
+        // this is the object ^^^^
+
+        System.out.println("congrats, we found your car! Here is your brand new " + foo.modelName + " " + foo.modelName + " " + foo.year + " with " + foo.milesDriven + " miles on it.");
+
+        scan.close();
+    }
+}
---
+++ 022_MaximumIntergers/MaximumIntegers.java
@@ -0,0 +1,20 @@
+public class MaximumIntegers {
+    public static void main(String[]args) {
+        Integer y = 0;
+        int Maximum = y.MAX_VALUE;
+        int Minimum = 745;
+        double randomNumber = (Math.random());
+
+        // System.out.println(y.MAX_VALUE);
+        // System.out.println(y.MIN_VALUE);
+        // System.out.println(randomNumber);
+        System.out.println(Maximum - 2146483647 + " is the maximum");
+        System.out.println(Minimum - 0 + " is the new minimum");
+    }
+}
+
+        
+
+//2147483647 - x = 1,000,000 
+// x = 2146483647
+// ^^ for max. number that will make 21474483647 minus x = 1,000,000
