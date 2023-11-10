dc49d07984a6251f09a38aeb3db3dba3767c6e91
Kat <nelson.katherine.24@c-dh.org>
2023-11-10 10:20:03 -0600

    30 and 32 done and updated

--- 030_EvenOrOdd/EvenOrOdd.java
+++ 030_EvenOrOdd/EvenOrOdd.java
@@ -9,7 +9,7 @@ public class EvenOrOdd
         System.out.println("what number do you choose?");
         num = scan.nextInt();
     
-    
+    // even bc if u divide it by two the ramineder should be zero
     if (num % 2==0)
         {
             System.out.println("The number you chose is even");
@@ -18,7 +18,7 @@ public class EvenOrOdd
     {
         System.out.println("The number you chose is odd");
     }
-
+// saying that if the remainder isnt t0, than it must be one which means its odd so else
     scan.close();
     }
 }
---
+++ 032_ShippingCalculator/ShippingCalculator.java
@@ -0,0 +1,47 @@
+//3.5, if 0 < w <=1
+//5.5, if 1 < w <= 3
+//8.5, if 3<w<=10
+//10.5, if 10< w < = 20
+
+import java.util.Scanner;
+public class ShippingCalculator
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int num = 0;
+
+        System.out.println("how much does your package weigh?");
+        num = scan.nextInt();
+    
+
+    if (0 < num  && num<= 1)
+        {
+            System.out.println("The cost to ship that package will be $3.50");
+        }
+    if(1 < num && num<=3)
+    {
+        System.out.println("The cost to ship your package is $5.50");
+    }
+    if(3 < num && num<=10)
+    {
+        System.out.println("The cost to ship your package is $8.50");
+    }
+    if(10 < num && num<=20)
+    {
+        System.out.println("The cost to ship your package is $8.50");
+    }
+    // this hadnles anything negative or invalid 
+    if (num<0)
+    {
+        System.out.println("The package you want to ship is invalid");
+    }
+    // this handles anyting overweihgt
+    if (num>=20)
+    {
+        System.out.println("The package you want to ship is too heavy");
+    }
+        scan.close();
+
+    }
+    }
