f3328831f2fb87d861ca6844d2c390dece3602ad
Kat <143726759+13katn@users.noreply.github.com>
2023-12-08 14:39:01 -0600

    drafts

---
+++ 39_VariousCounters/VariousCounters.java
@@ -0,0 +1,42 @@
+public class VariousCounters
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        System.out.println("cpounting one to one hundred:");
+        while(i<= 100)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+    int endPoint = 2000007;
+    int startingPoint = 103437;
+    while (startingPoint < endPoint){
+        if (startingPoint % 7 ==0){
+           System.out.println(startingPoint);
+        }
+        startingPoint ++;
+        }
+    }
+    }
+
+//while(103436<= i && i=<2000007);
+
+
+//x = 103436;
+//y = 2000007;
+
+// {
+//         int i=103437;
+//         System.out.println("cpounting one to one hundred:");
+//        // while(i<= 100)
+//        while(103436<i & i<2000007 && i%7==0 ){
+
+//         {
+//             System.out.println(i);
+//             i++;
+//         }
+//        }
+// }
+
