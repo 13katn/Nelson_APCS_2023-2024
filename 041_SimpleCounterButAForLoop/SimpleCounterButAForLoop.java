3b6023accb20d8a8dd285099fafd8d533b46d399
Kat <143726759+13katn@users.noreply.github.com>
2023-12-15 13:45:41 -0600

    simple counter 41

---
+++ 041_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
@@ -0,0 +1,36 @@
+public class SimpleCounterButAForLoop
+{
+    public static void main(String args[])
+    {
+        // from 0 to 300 
+         for( int i = 0; i < 301; i++)
+         {
+             System.out.println(i);
+         }
+         // from 300 to 0 
+          for( int i = 300 ; i > 0; i--)
+         {
+             System.out.println(i);
+         }
+         // counting up from 4798 and 5324
+          for(int i = 4798; i < 5325; i++)
+         {
+             System.out.println(i);
+         }
+         
+        }
+    }
+
+
+
+// EXAMPLE I FOUND ONLINE CHECK 
+// public static void main(String[] args) throws Exception {
+
+//     int start = 254;
+//     int end = 259;
+//     List<Integer> numberList = new ArrayList<Integer>();
+//     for(int i = start+1; i < end; i++) {
+//         //Prints the numbers exclusive...
+//         System.out.println(i);
+//         //Adds the numbers to the list
+//         numberList.add(i);      
