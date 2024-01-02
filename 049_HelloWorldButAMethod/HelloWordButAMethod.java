3f6e2730a80881cf71e97341d1e276000955b9ba
Kat <nelson.katherine.24@c-dh.org>
2024-01-01 22:21:12 -0600

    49 and 50

---
+++ 049_HelloWorldButAMethod/HelloWordButAMethod.java
@@ -0,0 +1,8 @@
+public class HelloWorldButAMethod{
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        helloWorld(); //  calls the hello World method
+    }
+}
---
+++ 050_NameGreeting/NameGreeting.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NameGreeting {
+    // Custom method to print the user entered name 
+    public static void printGreeting(String name) {
+        System.out.println("GReetings " + name + "!!!");
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+        System.out.print("Whats youre name: ");
+        String userName = scan.nextLine();
+        printGreeting(userName); // <-- calls the method that has the scanner
+        scan.close();
+    }
+}
