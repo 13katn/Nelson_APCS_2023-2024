38eea84a587ca422c8eabff813d0a7f44bf7f8b9
Kat <143726759+13katn@users.noreply.github.com>
2024-03-14 13:49:14 -0500

    70

--- 70_FavoritesTwo/FavoriteTVShowsTwo.java
+++ 70_FavoritesTwo/FavoriteTVShowsTwo.java
@@ -1,5 +1,13 @@
-package 70_FavoritesTwo;
-
+import java.util.ArrayList;
 public class FavoriteTVShowsTwo {
-    
+    public static void main(String[] args){
+        ArrayList<String> ftv = new ArrayList<String>();
+        ftv.add(new String("midnight gospel")); // addong numbers or like 0 , 2 ,2 is not needed fpr this 
+        ftv.add(new String("adventure time"));
+        ftv.add(new String("regular show or the simpsons its tied "));
+        //for loop tp call it back
+        for (int i = 0; i < ftv.size(); i++){
+            System.out.println(" my favoite tv shows are " + ftv.get(i)); // get not a setter becasue were not chaigning the lsit at all 
+        }
+    }
 }
