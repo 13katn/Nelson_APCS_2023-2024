d6e5efc0ffd72ecb5a83885a2a13bbd1fd90339c
Kat <nelson.katherine.24@c-dh.org>
2024-04-08 14:42:17 -0500

    work

--- 071_RollYourOwnDice/Dice.java
+++ 071_RollYourOwnDice/Dice.java
@@ -1,28 +1,21 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+public class Dice{
+    int numberSides;
+    int currentValue;
+    int random;
 
-class Dice {
-    private final int numberSides; // shows number of sides of the dice 
-    private int currentVal; // shows the value that the dice has currently
-    private final Random random; // rand num for ROLLING the dice 
-
-    // sets total num of sides thru constructor 
-    public Dice(int numberSides) {
-        this.numberSides = numberSides;
-        this.random = new Random();
-        roll(); // Rolls the dice after i created it in the last block
-    }
-
-// this is the current value accessable thru the getter 
-    public int getCurrentVal() {
-        return currentVal;
-    }
-
-// method that rolls nad returns (pretty efficiently i think?)
+   // sets total num of sides thru constructor
+   public Dice(int numberSides) {
+       this.numberSides = numberSides;
+       this.random = new Random();
+       roll(); // Rolls the dice after i created it in the last block
+   }
+   // this is the current value accessable thru the getter
+   public int getCurrentVal() {
+       return currentVal;
+   }
     public int roll() {
-        // rand number from 1-num of sides dice has 
-        currentVal = random.nextInt(numberSides) + 1;
-        return currentVal; 
-    }
+       // rand number from 1-num of sides dice has
+       currentVal = random.nextInt(numberSides) + 1;
+       return currentVal;
+   }
 }
