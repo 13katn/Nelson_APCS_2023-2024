152b3dc93622882a01d21e21a12c8b1df0c6f6b4
Kat <nelson.katherine.24@c-dh.org>
2024-04-12 08:55:37 -0500

    deck of cards done

--- 069_DeckOfCards/Card.java
+++ 069_DeckOfCards/Card.java
@@ -1,41 +1,18 @@
-// public class Card{
-//    // values
-// String suits;
-// String value;
-//    //method
-// public Card(String suits, String value){
-// this.value = value;
-// this.suits = suits;
-//    }
-// //getters / setters
-// public String getSuit(){
-//    return suits;
-// }  
-// public String getValue(){
-//    return value;
-// }
-// public static void main(String [] args){}
-  
-//    }
 public class Card{
-
     //values for cards and suites
 String suits;
 String value;
-    //method for cards and suits
+    //method
 public Card(String suits, String value){
 this.value = value; 
 this.suits = suits;
     }
-    
- //getters and setters for cards and suits
+ //getters and setters
 public String getSuit(){
     return suits;
- }
-    
+} 
 public String getValue(){
     return value;
 }
 public static void main(String [] args){}
-    
-    }
+}
