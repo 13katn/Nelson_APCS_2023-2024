3f90167651507186182d13064f54334bfba5abd4
Kat <nelson.katherine.24@c-dh.org>
2024-04-12 08:53:42 -0500

    cards

---
+++ 069_DeckOfCards/Card.java
@@ -0,0 +1,41 @@
+// public class Card{
+//    // values
+// String suits;
+// String value;
+//    //method
+// public Card(String suits, String value){
+// this.value = value;
+// this.suits = suits;
+//    }
+// //getters / setters
+// public String getSuit(){
+//    return suits;
+// }  
+// public String getValue(){
+//    return value;
+// }
+// public static void main(String [] args){}
+  
+//    }
+public class Card{
+
+    //values for cards and suites
+String suits;
+String value;
+    //method for cards and suits
+public Card(String suits, String value){
+this.value = value; 
+this.suits = suits;
+    }
+    
+ //getters and setters for cards and suits
+public String getSuit(){
+    return suits;
+ }
+    
+public String getValue(){
+    return value;
+}
+public static void main(String [] args){}
+    
+    }
--- 069_DeckOfCards/Table.java
+++ 069_DeckOfCards/Table.java
@@ -1,9 +1,16 @@
-package 069_DeckOfCards;
+public class Table{
 
-public class Table {
-    
+public static void main(String [] args){
+
+  Deck deck = new Deck();
+  Card[] deckOfCards = deck.getDeck();
+
+// for each loop getting value and suit from array and printing
+
+    for (Card card:deckOfCards){
+
+      System.out.println("the card is " + " " + card.getValue()  +  "" + " of " + " " + card.getSuit() );
+
+  }
 }
-
-/*
-    0/5 incomplete
-*/
+}
