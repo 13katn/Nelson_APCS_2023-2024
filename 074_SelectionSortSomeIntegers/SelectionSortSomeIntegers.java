76c9e4c5e816a4da5ac2e066921240501bd8d179
Kat <nelson.katherine.24@c-dh.org>
2024-04-17 14:38:51 -0500

    73 74

---
+++ 074_SelectionSortSomeIntegers/SelectionSortSomeIntegers.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class SelectionSortSomeIntegers {
+    public static void main(String[] args) {
+ System.out.println("How many random integers do you want to generate:");
+      int input = scan.nextInt(); 
+    scan.close();
+    }
+}
